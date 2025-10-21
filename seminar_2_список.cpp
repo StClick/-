1f755e1237e3e13f272da5ccf70c046769434f37
StClick <shtyrkov2016@list.ru>
2025-10-21 23:34:23 +0300

    Add files via upload

--- seminar_2_список.cpp
+++ seminar_2_список.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <queue>
+#include <stack>
 
 using namespace std;
 
@@ -6,6 +8,7 @@ using namespace std;
 struct Node
 {
     int value;
+    char c;
     Node *next;
 };
 
@@ -16,13 +19,18 @@ class LinkedList
     Node *tail;
     LinkedList();
     void addNewHead(int n);
+    void addNewHead_char(char n);
     void addNewTail(int n);
+    void addNewTail_char(char n);
     void printList();
+    void printList_char();
     bool hasCycle();
     void reverseList();
     Node *middleList();
     void eraseElement(int elem);
     void listUnite(Node *p);
+    bool isSubsequence(Node *p);
+    bool isPalindrome();
 };
 
 LinkedList :: LinkedList()
@@ -36,6 +44,22 @@ void LinkedList :: addNewHead(int n)
 {
     Node *p  = new Node;
     p->value = n;
+    if(head == NULL)
+    {
+        p->next = NULL;
+        tail = p;
+    }
+    else
+    {
+        p->next = head;
+    }
+    head = p;
+}
+
+void LinkedList :: addNewHead_char(char n)
+{
+    Node *p  = new Node;
+    p->c = n;
     if(head == NULL)
     {
         p->next = NULL;
@@ -65,6 +89,22 @@ void LinkedList :: addNewTail(int n)
     p->next = NULL;
 }
 
+void LinkedList :: addNewTail_char(char n)
+{
+    Node *p  = new Node;
+    p->c = n;
+    if(head == NULL)
+    {
+        head = p;
+    }
+    else
+    {
+        tail->next = p;
+    }
+    tail = p;
+    p->next = NULL;
+}
+
 
 //Вывод списка на экран (не для циклических списков!) 
 void LinkedList :: printList()
@@ -81,6 +121,31 @@ void LinkedList :: printList()
             else
             {
                 cout << p->value;
+            }
+            p = p->next;
+        }
+        cout << endl;
+    }
+    else
+    {
+        cout << "Список пуст" << endl;
+    }
+}
+
+void LinkedList :: printList_char()
+{
+    if(head!= NULL)
+    {
+        Node *p = head;
+        while(p)
+        {
+            if(p->next != NULL)
+            {
+                cout << p->c << "->";
+            }
+            else
+            {
+                cout << p->c;
             }
             p = p->next;
         }
@@ -175,6 +240,49 @@ void LinkedList :: eraseElement(int elem)
     head = dummy->next;
 }
 
+//Задача 5 "Является ли одна строка исходной для другой строки?"
+bool LinkedList :: isSubsequence(Node *p)
+{
+    queue<char> q;
+    Node *d;
+    d = head;
+    while(d != NULL)
+    {
+       q.push(d->c);
+       d = d->next;
+    }
+    
+    while(p != NULL)
+    {
+       if(q.front() == p->c) q.pop();
+       p = p->next;
+    }
+    
+    return q.size() == 0;
+}
+
+//Задача 6 "Является ли слово палиндромом?"
+bool LinkedList :: isPalindrome()
+{
+    stack<char> q;
+    Node *d;
+    d = head;
+    while(d != NULL)
+    {
+       q.push(d->c);
+       d = d->next;
+    }
+    d = head;
+    while(d != NULL)
+    {
+       if(q.top() != d->c) return false;
+       q.pop();
+       d = d->next;
+    }
+    
+    return true;
+}
+
 //Задача 7 "Слияние двух отсортированных списков"
 void LinkedList :: listUnite(Node *p)
 {
@@ -214,12 +322,13 @@ void LinkedList :: listUnite(Node *p)
 
 int main()
 {
-    LinkedList ob, ob2, ob3;
+    LinkedList ob, ob2, ob3, a, b, c, palindrom;
     ob.addNewHead(1);
     for(int i = 2; i <= 10; i++)
     {
         ob.addNewTail(i);
     }
+    
     cout << "Задача 1 'Есть ли циклы в списке?'" << endl;
     cout << "Для списка:";
     ob.printList();
@@ -229,15 +338,18 @@ int main()
     ob.hasCycle();
     ob.tail->next = NULL;
     cout << "/////////////////////////////////////////////////////////" << endl;
+    
     cout << "Задача 2 'Развернуть список' " << endl << "Было: ";
     ob.printList();
     ob.reverseList();
     cout << "Стало: ";
     ob.printList();
     cout << "/////////////////////////////////////////////////////////" << endl;
+    
     cout << "Задача 3 'Найти середину списка' " << endl;
     cout << "Средний элемент: " << ob.middleList()->value << endl;
     cout << "/////////////////////////////////////////////////////////" << endl;
+    
     cout << "Задача 4 'Удаление элемента из связного списка' " << endl;
     cout << "Удалим элемент со значением 5, тогда получим: ";
     ob.eraseElement(5);
@@ -254,6 +366,77 @@ int main()
     {
         ob2.addNewTail(i);
     }
+    cout << "/////////////////////////////////////////////////////////" << endl;
+    
+    cout << "Задача 5 'Является ли одна строка исходной для другой строки?' " << endl;
+    a.addNewHead_char('a');
+    a.addNewTail_char('b');
+    a.addNewTail_char('c');
+    a.addNewTail_char('d');
+    a.addNewTail_char('e');
+    a.addNewTail_char('f');
+    
+    b.addNewHead_char('c');
+    b.addNewTail_char('d');
+    b.addNewTail_char('e');
+    
+    c.addNewHead_char('b');
+    c.addNewTail_char('b');
+    c.addNewTail_char('c');
+    
+    cout << "Для строк вида:" << endl;
+    b.printList_char();
+    a.printList_char();
+    if(b.isSubsequence(a.head))
+    {
+       cout << "Первая является порождающей второй" << endl;
+    }
+    else
+    {
+       cout << "Первая не является порождающей второй" << endl;
+    }
+    cout << "Для строк вида:" << endl;
+    c.printList_char();
+    a.printList_char();
+    if(c.isSubsequence(a.head))
+    {
+       cout << "Первая является порождающей второй" << endl;
+    }
+    else
+    {
+       cout << "Первая не является порождающей второй" << endl;
+    }
+    cout << "/////////////////////////////////////////////////////////" << endl;
+    
+    cout << "Задача 6 'Является ли слово палиндромом?' " << endl;
+    palindrom.addNewHead_char('a');
+    palindrom.addNewTail_char('b');
+    palindrom.addNewTail_char('c');
+    palindrom.addNewTail_char('b');
+    palindrom.addNewTail_char('a');
+    
+    cout << "Строка:" << endl;
+    palindrom.printList_char();
+    if(palindrom.isPalindrome())
+    {
+       cout << "Палиндром" << endl;
+    }
+    else
+    {
+       cout << "Не палиндром" << endl;
+    }
+    cout << "Строка:" << endl;
+    a.printList_char();
+    if(a.isPalindrome())
+    {
+       cout << "Палиндром" << endl;
+    }
+    else
+    {
+       cout << "Не палиндром" << endl;
+    }
+    cout << "/////////////////////////////////////////////////////////" << endl;
+    
     cout << "Задача 7 'Слияние двух отсортированных списков' " << endl;
     cout << "Объединяем список1: ";
     ob.printList();
@@ -262,5 +445,6 @@ int main()
     ob.listUnite(ob2.head);
     cout << "Ответ: ";
     ob.printList();
+    
     return 0;
 }
